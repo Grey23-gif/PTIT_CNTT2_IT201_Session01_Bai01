ee4a56075b2ccf24b6d408045765843193659d8d
Grey23-gif <duong230426@gmail.com>
2025-06-30 22:02:04 +0700

    Bai_1

--- PTIT_CNTT2_IT201_Session01_Bai01/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    for (int i = 0; i < n; i++) { //O(n)
-        printf("Hello\n");  //O(1)
-    }
-    //Độ phức tạp thời gian của đoạn mã: O(n).
-    return 0;
-}
